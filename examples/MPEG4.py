7a6516379afa700a2160c3345804ce9e8b63d1d4
RBonna <ricardobonna@gmail.com>
2019-03-13 14:03:23 -0300

    Finishing implementing auxiliary functions for the MPEG4 example

--- examples/MPEG4.py
+++ examples/MPEG4.py
@@ -23,14 +23,14 @@ def idct(inp):
             else:
                 dct_matrix[i,j] = np.cos((2*j+1) * i * np.pi / (2*size[0]))
     dct_matrix = dct_matrix * np.sqrt(2/size[0])
-    return dct_matrix.transpose() * inp * dct_matrix
+    return dct_matrix.transpose() @ inp @ dct_matrix
 
 
-# block = (matrix, pos = (row,col)).
+# block = (matrix, pos = array[row,col]).
 def blockAdd(block, mat):
-    pos_start = (1,1)   # put (0,0) is you want the position to be 0 indexed
+    pos_start = np.array([1,1])   # put [0,0] is you want the position to be 0 indexed
     b_mat = block[0]
-    b_pos = (block[1][0]-pos_start[0], block[1][1]-pos_start[1])
+    b_pos = block[1]-pos_start
     b_size = b_mat.shape
     m_size = mat.shape
     result = np.zeros(m_size)
@@ -44,29 +44,53 @@ def blockAdd(block, mat):
 
 
 # Split a large block into a listo of macro blocks of size d = (dr,dc) or smaller
-def frame2mblocks(d,block):
+def frame2mblocks(d,frame):
     (dr,dc) = d
     result = []
     i = 0
-    while i < block.shape[0]:
+    while i < frame.shape[0]:
         j = 0
-        while j < block.shape[1]:
-            result.append((np.matrix(block[i:i+dr,j:j+dc]), (i+1,j+1)))
+        while j < frame.shape[1]:
+            result.append((frame[i:i+dr,j:j+dc], np.array([i+1,j+1])))
             j+=dc
         i+=dr
     return result
 
 
 # Get a matrix x and a list of motion vecotrs and return a motion compensated matrix
+# mvs = [(array(pos), array(mv))]
 def motionComp(mvs, x, bs):
-    return
+    x_size = x.shape
+    x = frame2mblocks((bs,bs), x)
+    mCompList1 = [(a[0], a[1]+b[1]) for a in x for b in mvs if np.array_equal(a[1], b[0])]
+    mCompList2 = [a for a in x if not any(map(lambda x: np.array_equal(a[1],x), [b[0] for b in mvs]))]
+    mCompList = mCompList1 + mCompList2
+    result = np.zeros(x_size)
+    for i in mCompList:
+        result = blockAdd(i,result)
+    return result
+
+
+# Gets a list of macro blocks [(matrix, pos = array[row,col])] and adds one by
+# one to the matrix frame at the position defined by pos
+def frameRC(mbl, frame):
+    for i in mbl:
+        frame = blockAdd(i,frame)
+    return frame
+
 
 # Test of the module
 if __name__ == '__main__':
     print("MPEG4 model\n")
-    print(idct(np.eye(3)))
+    # print(idct(np.eye(3)))
+    #
+    # a = np.eye(4)
+    # b = np.eye(2)
+    # print(blockAdd((b,np.array([0,0])),a))
+    # print(frame2mblocks((3,3),a))
 
-    a = np.eye(4)
-    b = np.eye(2)
-    print(blockAdd((b,(0,0)),a))
-    print(frame2mblocks((3,3),a))
+    a = np.around(10*np.random.rand(4,4))
+    print(a)
+    print('\n\n')
+    mvs = [(np.array([1,1]), np.array([1,0])), (np.array([3,3]), np.array([0,-1]))]
+    print(motionComp(mvs,a,2))
